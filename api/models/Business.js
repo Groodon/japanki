a4da1f3f22d036f09b79da1bf2243e74f4b08071
Groodon <simondelvert@hotmail.com>
2019-06-20 17:24:42 +0200

    toturial progress

---
+++ api/models/Business.js
@@ -0,0 +1,22 @@
+// require loads the module
+const mongoose = require('mongoose');
+// Each schema maps to a MongoDB collection and
+// defines the shape of the documents within that collection.
+const Schema = mongoose.Schema;
+
+// Define collection and schema for Business
+let Business = new Schema({
+  person_name: {
+    type: String
+  },
+  business_name: {
+    type: String
+  },
+  business_gst_number: {
+    type: Number
+  }
+},{
+  collection: 'business'
+});
+
+module.exports = mongoose.model('Business', Business);

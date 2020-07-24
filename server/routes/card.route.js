32791a6ed83e582516a2ac6b237f49616c53d217
Groodon <simondelvert@hotmail.com>
2020-07-24 12:57:31 +0200

    extracted jwt-auth

--- server/routes/card.route.js
+++ server/routes/card.route.js
@@ -3,34 +3,12 @@
 const express = require('express');
 const cardRoutes = express.Router();
 const cardController = require('../controllers/card.controller');
-const jwt = require('jsonwebtoken');
-
-const config = require('../config.json');
-const accessTokenSecret = config.secret;
-
-const authenticateJWT = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-
-  if (authHeader) {
-      const token = authHeader.split(' ')[1];
-
-      jwt.verify(token, accessTokenSecret, (err, user) => {
-          if (err) {
-              return res.sendStatus(403);
-          }
-
-          req.user = user;
-          next();
-      });
-  } else {
-      res.sendStatus(401);
-  }
-}
+const jwtAuth = require('../_helpers/jwt-authentication');
 
 // Defined store route
-cardRoutes.post('/', authenticateJWT, cardController.addCard);
-cardRoutes.put('/:id', authenticateJWT, cardController.updateCard);
-cardRoutes.delete('/:id', authenticateJWT, cardController.deleteCard);
+cardRoutes.post('/', jwtAuth, cardController.addCard);
+cardRoutes.put('/:id', jwtAuth, cardController.updateCard);
+cardRoutes.delete('/:id', jwtAuth, cardController.deleteCard);
 
 
 

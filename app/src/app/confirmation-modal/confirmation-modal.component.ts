53982d2fe3fcca33e66b95422586ee6bd9673ca0
Emil <emil_bergqvist@hotmail.com>
2020-07-26 12:02:40 +0200

    added interface

--- app/src/app/confirmation-modal/confirmation-modal.component.ts
+++ app/src/app/confirmation-modal/confirmation-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import IConfirmation from '../_models/confirmation-modal';
 
 @Component({
   selector: 'app-confirmation-modal',
@@ -8,7 +9,7 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class ConfirmationModalComponent implements OnInit {
 
-  constructor(public dialogRef: MatDialogRef<ConfirmationModalComponent>, @Inject(MAT_DIALOG_DATA) public data: any) { }
+  constructor(public dialogRef: MatDialogRef<ConfirmationModalComponent>, @Inject(MAT_DIALOG_DATA) public data: IConfirmation) { }
 
   ngOnInit() {
   }
